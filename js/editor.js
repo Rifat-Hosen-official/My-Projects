d85a82849c4328a629806224fd7edfe62676de6f
rifathosenofficial <rifathosenofficial@gmail.com>
2023-06-04 18:22:58 +0600

    simple project

---
+++ js/editor.js
@@ -0,0 +1,13 @@
+
+
+
+function f2(){
+    document.getElementById('textarea1').classList.toggle('text');
+}
+function f3(){
+    document.getElementById('textarea1').classList.toggle('italic');
+}
+function f4(){
+    document.getElementById('textarea1').classList.toggle('underline');
+}
+
